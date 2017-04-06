4388e76020fe139a8874e6249be93359f37aa4ea
Angelo Lu <luangelo.87@gmail.com>
2017-04-06 18:31:42 -0400

    Maintenance Commit

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,69 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
-import java.awt.BorderLayout;
 import java.util.Random;
 
-public class Main {
+public class Main{
+    static Random rand = new Random();
+    static int firstNum, secondNum;
+    static NumberGrid myNumberGrid;
 
-	
-	
-	public static void main(String[] args) {
-		
-		Random rand = new Random();
-		
-        int firstNum = rand.nextInt(9) + 1;
-        int secondNum = rand.nextInt(9) + 1;
-        
-        
-        int secretNum = firstNum * 10 + secondNum;
-        System.out.println(secretNum);
-        
-        JFrame frame = new JFrame("Pico-Fermi-Bagel Game");
-        frame.setSize(1600, 900);
-        frame.setLocationRelativeTo(null);
-        JPanel panel = new JPanel();
-        
-        JButton btn = new JButton();
-        btn.setSize(1000, 1000);
-        panel.add(btn);
+    public static void main(String[] args) {
+        JPanel mainPanel = new JPanel();
+        JPanel northPanel = new JPanel();
+        JPanel southPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        myNumberGrid = new NumberGrid();
+
+        JButton btnReset = new JButton();
+        JButton btnCheck = new JButton();
+
+        btnReset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkLogic();
+            }
+        });
+        btnCheck.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetGame();
+            }
+        });
         JTextField txt1 = new JTextField(20);
         JTextField txt2 = new JTextField(20);
-        panel.add(txt1);
-        panel.add(txt2);
-        
-        frame.add(panel);
+
+        northPanel.add(btnReset);
+        southPanel.add(txt1);
+        southPanel.add(txt2);
+        mainPanel.add(northPanel, BorderLayout.NORTH);
+        mainPanel.add(myNumberGrid,BorderLayout.CENTER);
+        mainPanel.add(southPanel, BorderLayout.SOUTH);
+
+        JFrame frame = new JFrame("Pico-Fermi-Bagel Game");
+        frame.setSize(500, 900);
+        frame.setLocationRelativeTo(null);
+        frame.add(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
         frame.setVisible(true);
-        
-        btn.addActionListener(new ActionListener() {
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		Logic l1 = new Logic(Integer.parseInt(txt1.getText()), firstNum, secondNum);
-                Logic l2 = new Logic(Integer.parseInt(txt2.getText()), secondNum, firstNum);
-                
-                System.out.println(l1.getState());
-                System.out.println(l2.getState());
-        	}
-        });
-        
-        
-        
+        resetGame();
+    }
+
+    private static void resetGame(){
+        firstNum = rand.nextInt(9) + 1;
+        secondNum = rand.nextInt(9) + 1;
+        while (firstNum == secondNum) {
+            secondNum = rand.nextInt(9) + 1;
+        }
+        System.out.println(firstNum * 10 + secondNum);
+    }
+    private static void checkLogic(){
+        //Logic l1 = new Logic(Integer.parseInt(txt1.getText()), firstNum, secondNum);
+        //Logic l2 = new Logic(Integer.parseInt(txt2.getText()), secondNum, firstNum);
+
+        //System.out.println(l1.getState());
+        //System.out.println(l2.getState());
     }
 }
