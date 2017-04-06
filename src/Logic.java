329e3f82ae028332e96161ddb6c36368e8bfe006
PfeifferH <15hp19@queensu.ca>
2017-04-06 17:51:53 -0400

    Game Logic

---
+++ src/Logic.java
@@ -0,0 +1,23 @@
+
+public class Logic {
+	
+	private int userNr; /* This i the user input for a particular place in the two digit number */
+	private int nr; /* This is the number in the same place as the user input */
+	private int secondNr; /* This is the number in the opposite place of the two digit number */
+	
+	public Logic(int userNr, int nr, int secondNr) {
+		this.userNr = userNr;
+		this.nr = nr;
+		this.secondNr = secondNr;
+	}
+
+	String getState() {
+		
+		if(userNr == nr) return "Fermi";
+		
+		else if (userNr == secondNr) return "Pico";
+		
+		else return "Bagel";
+	}
+	
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,52 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("A Swing app");
-        frame.setSize(300, 200);
+
+	
+	
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		
+        int firstNum = rand.nextInt(9) + 1;
+        int secondNum = rand.nextInt(9) + 1;
+        
+        
+        int secretNum = firstNum * 10 + secondNum;
+        System.out.println(secretNum);
+        
+        JFrame frame = new JFrame("Pico-Fermi-Bagel Game");
+        frame.setSize(1600, 900);
         frame.setLocationRelativeTo(null);
         JPanel panel = new JPanel();
-        frame.add(panel);
-        JButton btn = new JButton("A Button");
-        btn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button clicked");
-            }
-        });
+        
+        JButton btn = new JButton();
+        btn.setSize(1000, 1000);
         panel.add(btn);
+        JTextField txt1 = new JTextField(20);
+        JTextField txt2 = new JTextField(20);
+        panel.add(txt1);
+        panel.add(txt2);
+        
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        
+        btn.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		Logic l1 = new Logic(Integer.parseInt(txt1.getText()), firstNum, secondNum);
+                Logic l2 = new Logic(Integer.parseInt(txt2.getText()), secondNum, firstNum);
+                
+                System.out.println(l1.getState());
+                System.out.println(l2.getState());
+        	}
+        });
+        
+        
+        
     }
 }
